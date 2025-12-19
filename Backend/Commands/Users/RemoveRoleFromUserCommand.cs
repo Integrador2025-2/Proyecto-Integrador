402fc613d21a6b8fc5a7c5a5f62588285d4422a6
Erley Jhovan Cabrera <erley.1701920254@ucaldas.edu.co>
2025-12-19 14:31:36 -0500

    Remove Backend to replace from devDB

--- Backend/Commands/Users/RemoveRoleFromUserCommand.cs
+++
@@ -1,9 +0,0 @@
-using Backend.Models.DTOs;
-using MediatR;
-
-namespace Backend.Commands.Users;
-
-public class RemoveRoleFromUserCommand : IRequest<UserDto>
-{
-    public int UserId { get; set; }
-}
--- Backend/Models/DTOs/N8nIntegrationRequestDto.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Backend.Models.DTOs;
-
-public class N8nIntegrationRequestDto
-{
-    public List<CadenaDeValorDto> CadenaDeValor { get; set; } = new();
-    public List<ActividadDto> Actividades { get; set; } = new();
-}
